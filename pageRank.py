c53df173040bd5649f6fdbeff0745c9ef60b4911
electrostar121 <50092193+electrostar121@users.noreply.github.com>
2022-11-26 19:46:47 -0800

    Update pageRank.py

--- pageRank.py
+++ pageRank.py
@@ -1,15 +1,41 @@
-from sklearn.feature._extraction import DictVectorizer
+from sklearn.feature_extraction import DictVectorizer
 import pandas as pd
 
 test = {
 
-    "website1": ["website2", "website3", "website1"],
-    "website2": ["website2", "website3"],
-    "website3": []
+    "website3": ["website2", "website3", "website1"],
+    "website1": ["website2", "website3"],
+    "website2": []
 
 }
 
+listOfUrls = ["website1", "website2", "website3"]
+
+restructured = []
+keyValues = []
+
+flag = True
+i = 0
+
+for key in test:
+    data_dict = {}
+    for url in listOfUrls:
+        if url in test[key]:
+            data_dict[url] = 1
+        else:
+            data_dict[url] = 0
+            
+    restructured.append(data_dict)
+    keyValues.append(key)
+    
 dv = DictVectorizer()
+df = pd.DataFrame(dv.fit_transform(restructured).todense(), columns=dv.feature_names_, index=dict(enumerate(keyValues))).rename(index=dict(enumerate(keyValues)))
+    
+#print(df)
 
+#print(df.loc["website3", :])
 
-print(pd.DataFrame(dv.fit_transform(test).todense(), columns=dv.feature_names_))
+for item in keyValues:
+    for item2 in listOfUrls:
+        print(df.loc[item, item2])
+        
